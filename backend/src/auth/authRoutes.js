5c1903953b191abb68fdcdd6cbdbea6d7457761a
dev-syKRISHNA <s.yadhukrishna.menon@outlook.com>
2026-01-13 12:24:54 +0530

    Initial commit to the backend of  bookworm.expo app

---
+++ backend/src/auth/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router()
+
+router.post('/register',async (req, res) => {
+    res.send('Register')
+})
+router.post('/login',async (req, res) => {
+    res.send('Login')
+})
+
+export default router;
