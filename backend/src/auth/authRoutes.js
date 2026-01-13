30dbec7e486bc0fe0d2fa78552976d0ce904ccfa
dev-syKRISHNA <s.yadhukrishna.menon@outlook.com>
2026-01-13 17:07:00 +0530

    MongoDB connection

--- backend/src/auth/authRoutes.js
+++ backend/src/auth/authRoutes.js
@@ -3,7 +3,24 @@ import express from 'express';
 const router = express.Router()
 
 router.post('/register',async (req, res) => {
-    res.send('Register')
+    try {
+        const {username,email,password} = req.body;
+        if(!username || !email || !password){
+            return res.status(400).json({message: 'All fields are required'})
+        }
+        if(password.length < 6){
+            return res.status(400).json({message: 'Password must be at least 6 characters long'})
+        }
+        if(username.length < 3){
+            return res.status(400).json({message: 'Username must be at least 3 characters long'})
+        }
+
+
+        
+    } catch (error) {
+        
+    }
+
 })
 router.post('/login',async (req, res) => {
     res.send('Login')
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import "dotenv/config";
 
 import authRoutes from './auth/authRoutes.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB()
 });
