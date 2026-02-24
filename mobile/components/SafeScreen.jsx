116ae9fad17a06be510485fd9efbafd48ac2aa02
dev-syKRISHNA <s.yadhukrishna.menon@outlook.com>
2026-02-24 12:24:29 +0530

    seting up the frontend

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import COLORS from "../constants/colors";
 
-export default function SafeScreen() {
+export default function SafeScreen({ children }) {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text>SafeScreen</Text>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      {children}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+});
